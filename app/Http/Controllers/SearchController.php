4963fa399473e96a210c1af392f7865964a2fc36
Nick Kramer <nkramer@cwims.com>
2018-10-02 15:08:29 -0400

    added class images, character class + ilvl show up on search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Http\Lookups;
 use App\Realm;
 
 class SearchController extends Controller
@@ -12,7 +14,9 @@ class SearchController extends Controller
     public function search($region, $realm, $character)
     {
         $realms = Realm::all();
+        $character = Lookups::apiCharacter($character, $realm, $region);
+        $class_name = Lookups::classLookup($character->class);
 
-        return view('character', compact(['realms']));
+        return view('character', compact(['realms', 'character', 'class_name']));
     }
 }
--- resources/views/character.blade.php
+++ resources/views/character.blade.php
@@ -3,5 +3,14 @@
 @section('content')
     @include('partials.search')
 
-    CHARACTER THINGS
+    <div class="character-info">
+        <p class="character-name">{{ $character->name }}</p>
+        <img src="{{ asset('images').'/'.$class_name.'.png' }}"
+             alt="{{ $class_name }} symbol"
+             class="class-icon {{ $class_name }}"
+        />
+        <div class="item-level-container">
+            <p class="item-level">Item Level: <span class="item-level">{{ $character->items->averageItemLevel }}</span></p>
+        </div>
+    </div>
 @endsection
